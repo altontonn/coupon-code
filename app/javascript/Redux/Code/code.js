e72cd3338d2554c1dfea0bbd0a3292e331c4e943
Newton <altontonnalumasa@gmail.com>
2023-09-11 19:43:44 +0300

    Fetch data from API

--- app/javascript/Redux/Code/code.js
+++ app/javascript/Redux/Code/code.js
@@ -5,3 +5,15 @@ import axios from "axios";
 const initialState = {
   data: [],
 }
+
+const baseURL = `${window.location.origin}/api/v1`;
+export const fetchCodeData = createAsyncThunk(
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get(`${baseURL}/codes`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWitValue(e.response.data)
+    }
+  }
+)
