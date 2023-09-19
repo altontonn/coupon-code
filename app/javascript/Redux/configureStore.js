a5ed755cd68a0f924a04f2fdd831dc72fc42733d
Newton <altontonnalumasa@gmail.com>
2023-09-20 00:25:01 +0300

    Create store function to accept reducer

--- app/javascript/Redux/configureStore.js
+++ app/javascript/Redux/configureStore.js
@@ -1,5 +1,7 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, combineReducers, configureStore } from "@reduxjs/toolkit";
 import codeSlice from "./Code/code";
 const reducer = combineReducers({
   codes: codeSlice,
 })
+const store = configureStore(reducer, applyMiddleware(thunk))
+export default store;
