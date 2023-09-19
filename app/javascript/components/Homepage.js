6ea8339f3df6ec5f912c23e2b34d77a294a38125
Newton <altontonnalumasa@gmail.com>
2023-09-20 01:11:41 +0300

    Enable yarn build --watch

--- app/javascript/components/Homepage.js
+++ app/javascript/components/Homepage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { fetchCodeData } from "../Redux/Code/code";
+import { useDispatch } from "react-redux";
 const Homepage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCodeData())
+  }, [dispatch])
   return (
     <div>
       <h1>Homepage</h1>
