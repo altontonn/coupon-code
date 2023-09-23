494138a6f4c75ca4408b2802676e4e601d44c80c
Newton <altontonnalumasa@gmail.com>
2023-09-23 19:28:47 +0300

    import useEffect

--- app/javascript/components/Homepage.js
+++ app/javascript/components/Homepage.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import { fetchCodeData } from "../Redux/Code/code";
 import { useDispatch, useSelector } from "react-redux";
 const Homepage = () => {
   const dispatch = useDispatch();
   const codes = useSelector((state) => state.codes)
+
   useEffect(() => {
-    dispatch(fetchCodeData())
+    dispatch(fetchCodeData());
   }, [codes, dispatch])
+
   return (
     <div>
       <h1>Homepage</h1>
