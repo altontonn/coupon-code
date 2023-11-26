f6e21e17b67c3199dbdfb73267751cb77c5aa625
altontonn <altontonnalumasa@gmail.com>
2023-11-26 20:34:17 +0300

    Create form

---
+++ app/javascript/components/Form.js
@@ -0,0 +1,49 @@
+import React from "react";
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <div class="border-b border-gray-900/10 pb-12">
+            <h2 class="text-base font-semibold leading-7 text-gray-900">
+              Create a code
+            </h2>
+
+            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div class="sm:col-span-4">
+                <label
+                  for="username"
+                  class="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Code
+                </label>
+                <div class="mt-2">
+                  <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      type="text"
+                      name="username"
+                      id="username"
+                      autocomplete="username"
+                      class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      placeholder="pKJ02c"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-start gap-x-6">
+          <button
+            type="submit"
+            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default Form;
--- app/javascript/components/Homepage.js
+++ app/javascript/components/Homepage.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { fetchCodeData } from "../Redux/Code/code";
 import { useDispatch, useSelector } from "react-redux";
+import Form from "./Form";
 const Homepage = () => {
   const dispatch = useDispatch();
-  const codes = useSelector((state) => state.codes.codes)
+  const codes = useSelector((state) => state.codes.codes);
 
   useEffect(() => {
     console.log("Fetching data...");
@@ -17,17 +18,24 @@ const Homepage = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Your Data Codes</h1>
-      <ul className="divide-y divide-gray-200">
-      {codes && codes.length > 0 ? (
-        codes.map((code) => (
-          <li className="py-4 flex" key={code.id}>{code.code}</li>
-        ))
-      ) : (
-        <p>No codes available</p>
-      )}
-      </ul>
+    <div className="grid gap-x-8 gap-y-4 grid-cols-2">
+      <div>
+        <Form />
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold underline">Your Data Codes</h1>
+        <ul className="divide-y divide-gray-200">
+          {codes && codes.length > 0 ? (
+            codes.map((code) => (
+              <li className="py-4 flex" key={code.id}>
+                {code.code}
+              </li>
+            ))
+          ) : (
+            <p>No codes available</p>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
