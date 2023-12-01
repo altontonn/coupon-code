fb1b004a6dc72fa45e65f2cb9f4ec7ea97a2aebd
altontonn <altontonnalumasa@gmail.com>
2023-12-01 13:45:46 +0300

    Create the candidate component redeem codes

--- app/javascript/components/Candidate.js
+++ app/javascript/components/Candidate.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { fetchCodeData } from "../Redux/Code/code";
+import { useDispatch, useSelector } from "react-redux";
 
-const Researcher = () => {
-
-  return(
+const Candidate = () => {
+  const dispatch = useDispatch();
+  const codes = useSelector((state) => state.codes.codes);
+  useEffect(() => {
+    dispatch(fetchCodeData())
+  }, [dispatch])
+  return (
     <div>
-      <h1 className="text-3xl font-bold underline">Researcher</h1>
+      <h1 className="text-3xl font-bold underline">Redeem Codes</h1>
+      <ul className="divide-y">
+        {codes && codes.length > 0 ? (
+          codes.map((code) => (
+            <li className="py-4 flex" key={code.id}>
+              {code.passcode}
+              <button onClick={() => redeemCode(code.id)}>Redeem</button>
+            </li>
+          ))
+        ) : (
+          <p>No codes available</p>
+        )}
+      </ul>
     </div>
-  )
-}
-export default Researcher;
+  );
+};
+export default Candidate;
