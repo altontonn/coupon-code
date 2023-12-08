254c5c0efda1ffcc26a37e0911c333f69bda08e9
altontonn <altontonnalumasa@gmail.com>
2023-12-08 23:59:41 +0300

    Add reference to code

---
+++ db/migrate/20231208205318_add_user_to_codes.rb
@@ -0,0 +1,5 @@
+class AddUserToCodes < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :codes, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_08_203255) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_08_205318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_08_203255) do
     t.string "passcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_codes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_08_203255) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "codes", "users"
 end
