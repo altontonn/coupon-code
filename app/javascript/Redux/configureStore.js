6ce016abcac47325233a7cb4a186249de447c03e
altontonn <altontonnalumasa@gmail.com>
2023-12-22 00:01:39 +0300

    Import the authorizationslice

--- app/javascript/Redux/configureStore.js
+++ app/javascript/Redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import codeSlice from "./Code/code";
+import authorizationSlice from "./authorization";
 
 const store = configureStore({
   reducer: {
     codes: codeSlice,
+    authorization: authorizationSlice
   }
 })
 export default store;
