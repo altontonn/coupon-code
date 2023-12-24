d4d9ac56874376b71ebb6b2576b22fce28edf2b5
altontonn <altontonnalumasa@gmail.com>
2023-12-24 23:33:16 +0300

    Import the useSlice

--- app/javascript/Redux/configureStore.js
+++ app/javascript/Redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import codeSlice from "./Code/code";
+import userSlice from "./userSlice";
 import authorizationSlice from "./authorization";
 
 const store = configureStore({
   reducer: {
     codes: codeSlice,
+    user: userSlice,
     authorization: authorizationSlice
   }
 })
