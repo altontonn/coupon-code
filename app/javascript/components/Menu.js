19bb7e59c347daee2dcb16b4fb86048bb7d9c049
altontonn <altontonnalumasa@gmail.com>
2023-12-28 00:23:26 +0300

    Add a message to display the logged in admin

--- app/javascript/components/Menu.js
+++ app/javascript/components/Menu.js
@@ -36,6 +36,9 @@ const Menu = () => {
   };
   return (
     <ul className="mb-6 flex items-center justify-center flex-wrap bg-teal-500 p-6">
+      {user.success && (
+        <p className="text nav-text">Welcome {user.name}</p>
+      )}
       <NavLink
         to="/"
         className="mr-6 font-semibold text-teal-200 hover:text-white"
