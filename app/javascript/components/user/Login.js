d1a106e22e9c5a8c7a3962b42da6531117219ab2
altontonn <altontonnalumasa@gmail.com>
2024-01-02 17:17:37 +0300

    Display the button and import bootstrap cdn

--- app/javascript/components/user/Login.js
+++ app/javascript/components/user/Login.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../../Redux/userSlice";
+import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import { Alert } from "react-bootstrap";
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
@@ -37,26 +39,47 @@ const LoginForm = () => {
       });
   };
   return (
-    <Form onSubmit={handleSubmit}>
-      {errorMessage && <p style={{color: 'red', fontSize: '0.8em'}}>{errorMessage}</p>}
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-        <Form.Label column sm="2">
-          Email: 
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleChange} required/>
-        </Col>
-      </Form.Group>
+    <div className="d-flex justify-content-center">
+      <Form onSubmit={handleSubmit} className="w-50">
+        {errorMessage && (
+          <p style={{ color: "red", fontSize: "0.8em" }}>{errorMessage}</p>
+        )}
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+          <Form.Label column sm="2">
+            Email:
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </Col>
+        </Form.Group>
 
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-        <Form.Label column sm="2">
-          Password
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange} required/>
-        </Col>
-      </Form.Group>
-    </Form>
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+          <Form.Label column sm="2">
+            Password
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </Col>
+        </Form.Group>
+        <button type="button" class="btn btn-primary">
+          Primary
+        </button>
+      </Form>
+    </div>
   );
 };
 export default LoginForm;
