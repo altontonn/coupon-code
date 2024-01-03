b001e2981580d48a8c72b88550ea65d5ceca0b54
altontonn <altontonnalumasa@gmail.com>
2024-01-03 23:12:35 +0300

    Create a link for login

--- app/javascript/components/user/Registration.js
+++ app/javascript/components/user/Registration.js
@@ -1,44 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
   return (
-    <div className="d-flex justify-content-center">
-      <form className="row g-3 w-50">
-        <div className="col-md-12">
-          <label for="inputEmail4" className="form-label">
-            Name
-          </label>
-          <input type="text" className="form-control" name="name" />
-        </div>
-        <div className="col-md-12">
-          <label for="inputEmail4" className="form-label">
-            Email
-          </label>
-          <input type="email" className="form-control" name="email" />
-        </div>
-        <div className="col-md-12">
-          <label for="inputPassword4" className="form-label">
-            Password
-          </label>
-          <input type="password" className="form-control" name="password" />
-        </div>
-        <div className="col-md-12">
-          <label for="inputPassword4" className="form-label">
-            Re-Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            name="password_confirmation"
-          />
-        </div>
-        <div className="col-12">
-          <button type="submit" className="btn btn-primary">
-            Register
-          </button>
-        </div>
-      </form>
-    </div>
+    <>
+      <div className="d-flex justify-content-center">
+        <form className="row g-3 w-50">
+          <div className="col-md-12">
+            <label for="inputEmail4" className="form-label">
+              Name
+            </label>
+            <input type="text" className="form-control" name="name" />
+          </div>
+          <div className="col-md-12">
+            <label for="inputEmail4" className="form-label">
+              Email
+            </label>
+            <input type="email" className="form-control" name="email" />
+          </div>
+          <div className="col-md-12">
+            <label for="inputPassword4" className="form-label">
+              Password
+            </label>
+            <input type="password" className="form-control" name="password" />
+          </div>
+          <div className="col-md-12">
+            <label for="inputPassword4" className="form-label">
+              Re-Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              name="password_confirmation"
+            />
+          </div>
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+      <p className="text-center">
+        Already have an account? <Link className="text-decoration-none" to="/login">Login Here</Link>
+      </p>
+    </>
   );
 };
 export default RegisterForm;
