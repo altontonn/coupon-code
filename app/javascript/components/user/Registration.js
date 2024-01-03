301308dab034db035f92272c58cc332c995fbe80
altontonn <altontonnalumasa@gmail.com>
2024-01-03 23:21:48 +0300

    Created a user registration component using React hooks and Redux to manage state. Integrated registration functionality with react-router-dom for navigation. Utilized the registerUser action from the userSlice in Redux for handling registration logic.

--- app/javascript/components/user/Registration.js
+++ app/javascript/components/user/Registration.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { registerUser } from "../../Redux/userSlice";
 
 const RegisterForm = () => {
   return (
